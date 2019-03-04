bf5e137702eca467680f224bf2d09446b4cfd7ff
OlivierL-KRG <olivier.larue@krgcorporate.com>
2019-03-04 14:27:33 +0100

    Add configuration files (git, ide, eslint...).

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  'extends': 'airbnb-base',
+  'env': {
+    'browser': true,
+    'node': true
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        'max-len': 'off',
+        'no-param-reassign': 0,
+        'class-methods-use-this': 0,
+      }
+    }
+  ]
+};
