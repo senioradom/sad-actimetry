731279316d904088c9ebfd65e7541c7213ace8bb
OlivierL-KRG <olivier.larue@krgcorporate.com>
2019-03-04 14:38:25 +0100

    Add i18n.

---
+++ src/javascripts/model/I18n.js
@@ -0,0 +1,12 @@
+class I18n {
+  constructor() {
+    this.strings = {
+      fr: {
+        temperatures_24h: 'Temperatures (24h)',
+        temperatures_week: 'Temperatures (semaines)',
+      },
+    };
+  }
+}
+
+export default (new I18n());
